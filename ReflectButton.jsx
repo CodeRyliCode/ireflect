b31825a60d601293d83939c1d7b7f1feb7475856
Ryli Davis <rylidavis@Rylis-MacBook-Air.local>
2026-02-07 01:04:49 -0800

    add text area and button states

--- ReflectButton.jsx
+++ ReflectButton.jsx
@@ -1,9 +1,27 @@
 const ReflectButton = () => {
+const [openTextArea, setOpenTextArea] = React.useState(false)
+const [entry, setEntry] = React.useState("")
+    const onClickReflectButton = () => {
+    setOpenTextArea(true)
+    }
+
+    const onClickSaveButton = () => {
+        setOpenTextArea(false)
+    }
 
     return (
-        <div className={"button-container"}><button className={"reflect-button"}>
-            Reflect It!
-        </button>
+        <div className={"reflect-button-container"}>
+            {openTextArea ?
+                <><textarea className={"text-area"} value={entry} placeholder={"type here"} onChange={(e) => setEntry(e.target.value)}/>
+                    <div className={"save-button-container"}>
+                <button title={"save-that-thought"} className={"action-button"} onClick={onClickSaveButton}>Save that thought! :)</button>
+                </div></>
+                :
+            <button title={"reflect-it"} onClick={onClickReflectButton} className={"action-button"}>
+            Reflect it...
+        </button>}
+
+
         </div>
     )
 }
